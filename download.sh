e9fc05590e40ef073f1a0468439739b7264ee687
Dhanushreddy2002 <59830141+Dhanushreddy2002@users.noreply.github.com>
2024-01-13 16:14:17 +0000

    Add Code

--- download.sh
+++ download.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 VID_URL=$(curl -s "$QUERY_URL")
-VID_TITLE=$(youtube-dl --get-title "$VID_URL" | sed 's/[/\:*?"<>|]/_/g')
-python -m youtube_dl -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' -o "videos/${VID_TITLE}.mp4" --merge-output-format mp4 "$VID_URL"
+# VID_TITLE=$(youtube-dl --get-title "$VID_URL" | sed 's/[/\:*?"<>|]/_/g')
+# python -m youtube_dl -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' -o "videos/${VID_TITLE}.mp4" --merge-output-format mp4 "$VID_URL"
+python -m youtube_dl -f 'bestvideo[height<=1080]+bestaudio/best[height<=1080]' -o "output.mp4" --merge-output-format mp4 "$VID_URL"
